63b2aeda7131966b1cd5d7d2e7307010c78eeb6e
Swathi <swathy.m.ram@gmail.com>
2026-07-04 16:05:11 +0530

    loads huggingface and wraps with cache

---
+++ embeddings.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from langchain_classic.embeddings.cache import CacheBackedEmbeddings
+from langchain_classic.storage.file_system import LocalFileStore
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+from config import CONFIG
+
+
+@st.cache_resource
+def load_embeddings():
+    base_embeddings = HuggingFaceEmbeddings(
+        model_name=CONFIG.embedding_model_name
+    )
+    store = LocalFileStore(CONFIG.embed_cache_dir)
+    return CacheBackedEmbeddings.from_bytes_store(
+        underlying_embeddings=base_embeddings,
+        document_embedding_cache=store,
+        namespace=CONFIG.embedding_namespace,
+    )
