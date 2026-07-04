845c6196f3e251ff60390ade15ccf39ee079fca1
Swathi <swathy.m.ram@gmail.com>
2026-07-04 16:07:08 +0530

    loads groq llm and cache

---
+++ llm.py
@@ -0,0 +1,17 @@
+import os
+
+import streamlit as st
+from langchain_community.cache import SQLiteCache
+from langchain_core.globals import set_llm_cache
+from langchain_groq import ChatGroq
+
+from config import CONFIG
+
+
+@st.cache_resource
+def load_llm():
+    set_llm_cache(SQLiteCache(database_path=CONFIG.llm_cache_file))
+    return ChatGroq(
+        api_key=os.environ["GROQ_API_KEY"],
+        model=CONFIG.groq_model_name,
+    )
